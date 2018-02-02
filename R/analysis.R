28b7308f531531e45610e6ac5debe71334362fab
pli888 <peter@gigasciencejournal.com>
2018-02-02 09:51:10 +0800

    Display data in stacked bar chart

--- R/analysis.R
+++ R/analysis.R
@@ -13,40 +13,72 @@ ncol(gigatags)
 [1] 68
 
 # Reading data from GigaScience tab delimited file will fail
-gigadata <- read.table("data/wos/savedrecs_gigascience_289all.txt", sep="\t", nrows=1, skip=1, stringsAsFactors=FALSE)
+giga <- read.table("data/wos/savedrecs_gigascience_289all.txt", sep="\t", nrows=1, skip=1, stringsAsFactors=FALSE)
 # Converted data/wos/savedrecs_gigascience_289all.txt into CSV file using Excel
 
 # Try read data again
 giga <- read.csv("data/wos/savedrecs_gigascience_289all.csv", sep=",", stringsAsFactors=FALSE)
+# Number of papers
+nrow(giga)
+[1] 289
 
-# Remove papers which are Correction articles
+# Remove Correction articles
 giga <- giga[!grepl("Correction", giga$DT),]
 
 # Number of papers published since 2012
 nrow(giga)
 [1] 278
+# Therefore 11 correction articles
 
-# Get number of papers per year
+# Get total number of GigaScience papers per year
 num_pub_per_year <- giga$PY
 num_pub_per_year <- table(num_pub_per_year)
 # Convert to data frame
 num_pub_per_year <- as.data.frame(num_pub_per_year)
+years <- num_pub_per_year[,1]
+num_pub_per_year <- num_pub_per_year[,2]
 # Update column names
-colnames(num_pub_per_year) <- c("Year", "No. publications")
+#colnames(num_pub_per_year) <- c("Year", "No. publications")
+
+# Get all BGI papers from GigaScience using "BGI" in author addresses
+giga_bgi_idx <- grep("BGI", giga$C1, ignore.case = TRUE)
+# Select these BGI rows from giga data
+giga_bgi_papers <- subset(giga[giga_bgi_idx,],)
+# Get number of BGI papers per year
+num_BGI_pub_per_year <- giga_bgi_papers$PY
+num_BGI_pub_per_year <- table(num_BGI_pub_per_year)
+# Convert to data frame
+num_BGI_pub_per_year <- as.data.frame(num_BGI_pub_per_year)
+num_BGI_pub_per_year <- num_BGI_pub_per_year[,2]
+# Update column names
+#colnames(num_BGI_pub_per_year) <- c("Year", "No. publications")
+
+# Get other papers in gigascience journal
+giga_not_bgi_idx <- grep("BGI", giga$C1, ignore.case = TRUE, invert = TRUE)
+# Select these not BGI rows from giga data
+giga_not_bgi_papers <- subset(giga[giga_not_bgi_idx, ], )
+# Get number of not BGI papers per year
+num_not_BGI_pub_per_year <- giga_not_bgi_papers$PY
+num_not_BGI_pub_per_year <- table(num_not_BGI_pub_per_year)
+# Convert to data frame
+num_not_BGI_pub_per_year <- as.data.frame(num_not_BGI_pub_per_year)
+num_not_BGI_pub_per_year <- num_not_BGI_pub_per_year[,2]
 
 # Visualise results
-x <- num_pub_per_year$Year
-y <- num_pub_per_year[,2]
-text <- c('', '', '')
-data <- data.frame(x, y, text)
-
-p <- plot_ly(data, x = ~x, y = ~y, type = 'bar',
-text = y, textposition = 'auto',
-marker = list(color = 'rgb(158,202,225)',
-line = list(color = 'rgb(8,48,107)', width = 1.5))) %>%
-layout(title = "Number of publications per year in GigaScience",
+data <- data.frame(years, num_BGI_pub_per_year, num_not_BGI_pub_per_year)
+p <- plot_ly(data,
+x = ~years,
+y = ~num_BGI_pub_per_year,
+type = 'bar',
+text = num_BGI_pub_per_year, textposition = 'auto',
+name = 'BGI papers') %>%
+add_trace(y = ~num_not_BGI_pub_per_year,
+text = num_pub_per_year, textposition = 'auto',
+name = 'Non-BGI papers') %>%
+layout(title = "Number of publications with BGI in author list per year in GigaScience",
 xaxis = list(title = "Year"),
-yaxis = list(title = "No. of publications"))
+yaxis = list(title = 'No. papers'),
+barmode = 'stack')
 
 ####
 
@@ -68,34 +100,6 @@ colnames(top10_mostcited) <- c("Title", "First Author", "Publication Year", "Tot
 stargazer(top10_mostcited, type = "html", summary = FALSE, rownames = FALSE)
 
 ####
-
-# Get all BGI papers from GigaScience
-# Get all gigasci papers with "BGI" in author addresses
-giga_bgi_idx <- grep("BGI", giga$C1, ignore.case = TRUE)
-# Select these BGI rows from giga data
-giga_bgi_papers <- subset(giga[giga_bgi_idx,],)
-# Get number of BGI papers per year
-num_BGI_pub_per_year <- giga_bgi_papers$PY
-num_BGI_pub_per_year <- table(num_BGI_pub_per_year)
-
-# Convert to data frame
-num_BGI_pub_per_year <- as.data.frame(num_BGI_pub_per_year)
-# Update column names
-colnames(num_BGI_pub_per_year) <- c("Year", "No. publications")
-
-# Visualise results
-x <- num_BGI_pub_per_year$Year
-y <- num_BGI_pub_per_year[,2]
-text <- c('', '', '')
-data <- data.frame(x, y, text)
-
-p <- plot_ly(data, x = ~x, y = ~y, type = 'bar',
-text = y, textposition = 'auto',
-marker = list(color = 'rgb(158,202,225)',
-line = list(color = 'rgb(8,48,107)', width = 1.5))) %>%
-layout(title = "Number of publications with BGI in author list per year in GigaScience",
-xaxis = list(title = "Year"),
-yaxis = list(title = "No. of publications"))
 
 # Get top 10 most cited papers in GigaScience with BGI author list
 # Need to get hold of DOIs from BGI papers
